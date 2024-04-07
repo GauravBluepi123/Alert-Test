7a741e5aa0add1ca5a1229b8ce4faed2a789e202
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-07 18:04:35 +0530

    gg

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -8,7 +8,7 @@ resource "azurerm_storage_account" "StorageAccountDemo" {
   name                     = "sate"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
-  account_tier             = "Free"
+  account_tier             = "Standard"
   account_replication_type = "GRS"
 
 
