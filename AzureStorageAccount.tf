0dfe07bd8354e505cd40bebc4aa6146d5936c769
GauravBluepi123 <101235561+GauravBluepi123@users.noreply.github.com>
2024-04-07 18:43:01 +0530

    Update AzureStorageAccount.tf

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -3,13 +3,41 @@ resource "azurerm_resource_group" "demo" {
   location = "CentralIndia"
 }
 
-##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
   name                     = "sate"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+}
 
+resource "azurerm_monitor_action_group" "ActionGroupDemo" {
+  name                = "example-action-group"
+  resource_group_name = azurerm_resource_group.demo.name
+  short_name          = "example-action-group"
+  email_receiver {
+    name  = "email"
+    email = "gauravkumar.pandey@bluepi.in"
+  }
+}
 
+resource "azurerm_monitor_metric_alert" "storage_account_alert" {
+  name                = "storage-account-capacity-alert"
+  resource_group_name = azurerm_resource_group.demo.name
+  scopes              = [azurerm_storage_account.StorageAccountDemo.id]
+
+  criteria {
+    metric_namespace = "Microsoft.Storage/storageAccounts"
+    metric_name      = "BlobCapacity"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 1
+    time_aggregation = "Average"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.ActionGroupDemo.id
+  }
+
+  description = "Alert triggered when Blob capacity exceeds 80%"
 }
