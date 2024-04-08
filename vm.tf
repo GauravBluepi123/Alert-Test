0e27be9de4b4266e6f8c41d3b028a3460de9b1b8
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-08 14:54:11 +0530

    gg

--- vm.tf
+++ vm.tf
@@ -2,6 +2,44 @@ data "azurerm_resource_group" "storage_account_rg" {
   name = azurerm_storage_account.StorageAccountDemo.resource_group_name
 }
 
+
+resource "azurerm_virtual_network" "example" {
+  name                = "my-virtual-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.storage_account_rg.location
+  resource_group_name = data.azurerm_resource_group.storage_account_rg.name
+}
+
+# Define the subnet within the virtual network
+resource "azurerm_subnet" "example" {
+  name                 = "my-subnet"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.storage_account_rg.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Define the public IP address
+resource "azurerm_public_ip" "example" {
+  name                = "my-public-ip"
+  location            = data.azurerm_resource_group.storage_account_rg.location
+  resource_group_name = data.azurerm_resource_group.storage_account_rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Define the network interface
+resource "azurerm_network_interface" "example" {
+  name                = "my-network-interface"
+  location            = data.azurerm_resource_group.storage_account_rg.location
+  resource_group_name = data.azurerm_resource_group.storage_account_rg.name
+
+  ip_configuration {
+    name                          = "my-ip-config"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
 resource "azurerm_virtual_machine" "example" {
   name                  = "my-vm"
   location              = data.azurerm_resource_group.storage_account_rg.location
